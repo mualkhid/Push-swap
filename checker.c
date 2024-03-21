5faaed426d2b1354f0eb6e30f34798864a7c99f5
mualkhid <162097353+mualkhid@users.noreply.github.com>
2024-03-21 18:25:49 +0000

    21-03-2024

--- checker.c
+++ checker.c
@@ -10,120 +10,69 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "push_swap.h"
 
-int	main(int ac, char **av)
+void process_input(int ac, char **av, t_stack **a)
 {
-    t_stack	*a;
-    t_stack	*b;
-    char	*next_line;
-    char    **split_av;
-    int		len;
-    int     i;
+    char **split_av;
+    int i;
 
-    a = NULL;
-    b = NULL;
-    next_line = NULL;
-    if (ac == 1 || (ac == 2 && !av[1][0]))
-        return (0);
-    else
+    i = 1;
+    while (i < ac)
     {
-        i = 1;
-        while (i < ac)
+        split_av = split(av[i], ' ');
+        if (init_stack_a(a, split_av) == -1)
         {
-            split_av = split(av[i], ' ');
-            if (init_stack_a(&a, split_av) == -1)
-            {
-                free_split_array(split_av);
-                continue;
-            }
             free_split_array(split_av);
-            i++;
+            continue;
         }
+        free_split_array(split_av);
+        i++;
     }
-    len = stack_len(a);
+}
+
+void process_commands(t_stack **a, t_stack **b)
+{
+    char *next_line;
+
+    next_line = NULL;
     next_line = (get_next_line(0, next_line));
     while (next_line)
     {
-        apply_command(&a, &b, next_line);
+        apply_command(a, b, next_line);
         free(next_line);
         next_line = (get_next_line(0, next_line));
     }
+}
+
+void print_result(t_stack *a)
+{
+    int len;
+
+    len = stack_len(a);
     if (stack_sorted(a) && stack_len(a) == len)
         write(1, "OK\n", 3);
     else
         write(1, "KO\n", 3);
+}
+
+int main(int ac, char **av)
+{
+    t_stack *a;
+    t_stack *b;
+
+    a = NULL;
+    b = NULL;
+    if (ac == 1 || (ac == 2 && !av[1][0]))
+        return (0);
+    process_input(ac, av, &a);
+    process_commands(&a, &b);
+    print_result(a);
     free_stack(&a);
     free_stack(&b);
     return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int	main(int ac, char **av)
-// {
-// 	t_stack	*a;
-// 	t_stack	*b;
-// 	char	*next_line;
-// 	char	**split_av;
-// 	int		len;
-// 	int		i;
-// 	int		j;
-// 	int k;
-
-// 	split_av = NULL;
-// 	a = NULL;
-// 	b = NULL;
-// 	next_line = NULL;
-// 	k = 0;
-// 	if (ac == 1 || (ac == 2 && !av[1][0]))
-// 		return (0);
-// 	else
-// 	{
-// 		i = 1;
-// 		while (i < ac)
-// 		{
-// 			split_av = split(av[i], ' ');
-// 			j = 0;
-// 			while (split_av[j] != NULL)
-// 			{
-// 				init_stack_checker(&a, split_av[j]);
-// 				free(split_av[j]);
-// 				j++;
-// 			}
-// 			free(split_av);
-// 			i++;
-// 		}
-// 	}
-// 	len = stack_len(a);
-// 	while (next_line != NULL || k == 0)
-// 	{
-// 		next_line = (get_next_line(0, next_line));
-// 		apply_command(&a, &b, next_line);
-// 		free(next_line);
-// 		k++;
-// 	}
-// 	free(next_line);
-// 	if (stack_sorted(a) && stack_len(a) == len)
-// 		write(1, "OK\n", 3);
-// 	else
-// 		write(1, "KO\n", 3);
-// 	free_stack(&a);
-// 	free_stack(&b);
-// 	return (0);
-// }
 
 // #include "push_swap.h"
 
@@ -132,41 +81,156 @@ int	main(int ac, char **av)
 //     t_stack	*a;
 //     t_stack	*b;
 //     char	*next_line;
-//     char    **split_av = NULL;
+//     char    **split_av;
 //     int		len;
 //     int     i;
 
 //     a = NULL;
 //     b = NULL;
 //     next_line = NULL;
-//     if (ac == 1)
-//         return (1);
-//     else if (ac >= 2)
+//     if (ac == 1 || (ac == 2 && !av[1][0]))
+//         return (0);
+//     else
 //     {
-//         split_av = split(av[1], ' ');
-//         av = split_av;
+//         i = 1;
+//         while (i < ac)
+//         {
+//             split_av = split(av[i], ' ');
+//             if (init_stack_a(&a, split_av) == -1)
+//             {
+//                 free_split_array(split_av);
+//                 continue;
+//             }
+//             free_split_array(split_av);
+//             i++;
+//         }
 //     }
-//     init_stack_a(&a, av + 1);
 //     len = stack_len(a);
 //     next_line = (get_next_line(0, next_line));
 //     while (next_line)
 //     {
 //         apply_command(&a, &b, next_line);
+//         free(next_line);
 //         next_line = (get_next_line(0, next_line));
 //     }
 //     if (stack_sorted(a) && stack_len(a) == len)
 //         write(1, "OK\n", 3);
 //     else
 //         write(1, "KO\n", 3);
-//     free(a);
-//     if (split_av != NULL)
-//     {
-//         i = 0;
-//         while (split_av[i] != NULL) {
-//             free(split_av[i]);
-//             i++;
-//         }
-//         free(split_av);
-//     }
+//     free_stack(&a);
+//     free_stack(&b);
 //     return (0);
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // int	main(int ac, char **av)
+// // {
+// // 	t_stack	*a;
+// // 	t_stack	*b;
+// // 	char	*next_line;
+// // 	char	**split_av;
+// // 	int		len;
+// // 	int		i;
+// // 	int		j;
+// // 	int k;
+
+// // 	split_av = NULL;
+// // 	a = NULL;
+// // 	b = NULL;
+// // 	next_line = NULL;
+// // 	k = 0;
+// // 	if (ac == 1 || (ac == 2 && !av[1][0]))
+// // 		return (0);
+// // 	else
+// // 	{
+// // 		i = 1;
+// // 		while (i < ac)
+// // 		{
+// // 			split_av = split(av[i], ' ');
+// // 			j = 0;
+// // 			while (split_av[j] != NULL)
+// // 			{
+// // 				init_stack_checker(&a, split_av[j]);
+// // 				free(split_av[j]);
+// // 				j++;
+// // 			}
+// // 			free(split_av);
+// // 			i++;
+// // 		}
+// // 	}
+// // 	len = stack_len(a);
+// // 	while (next_line != NULL || k == 0)
+// // 	{
+// // 		next_line = (get_next_line(0, next_line));
+// // 		apply_command(&a, &b, next_line);
+// // 		free(next_line);
+// // 		k++;
+// // 	}
+// // 	free(next_line);
+// // 	if (stack_sorted(a) && stack_len(a) == len)
+// // 		write(1, "OK\n", 3);
+// // 	else
+// // 		write(1, "KO\n", 3);
+// // 	free_stack(&a);
+// // 	free_stack(&b);
+// // 	return (0);
+// // }
+
+// // #include "push_swap.h"
+
+// // int	main(int ac, char **av)
+// // {
+// //     t_stack	*a;
+// //     t_stack	*b;
+// //     char	*next_line;
+// //     char    **split_av = NULL;
+// //     int		len;
+// //     int     i;
+
+// //     a = NULL;
+// //     b = NULL;
+// //     next_line = NULL;
+// //     if (ac == 1)
+// //         return (1);
+// //     else if (ac >= 2)
+// //     {
+// //         split_av = split(av[1], ' ');
+// //         av = split_av;
+// //     }
+// //     init_stack_a(&a, av + 1);
+// //     len = stack_len(a);
+// //     next_line = (get_next_line(0, next_line));
+// //     while (next_line)
+// //     {
+// //         apply_command(&a, &b, next_line);
+// //         next_line = (get_next_line(0, next_line));
+// //     }
+// //     if (stack_sorted(a) && stack_len(a) == len)
+// //         write(1, "OK\n", 3);
+// //     else
+// //         write(1, "KO\n", 3);
+// //     free(a);
+// //     if (split_av != NULL)
+// //     {
+// //         i = 0;
+// //         while (split_av[i] != NULL) {
+// //             free(split_av[i]);
+// //             i++;
+// //         }
+// //         free(split_av);
+// //     }
+// //     return (0);
+// // }
--- stack_init.c
+++ stack_init.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "push_swap.h"
 
 void free_split_array(char **split_array)
@@ -23,7 +24,7 @@ void free_split_array(char **split_array)
     free(split_array);
 }
 
-long	ft_atol(const char *s)
+static long	ft_atol(const char *s)
 {
 	long	result;
 	int		sign;
@@ -44,7 +45,7 @@ long	ft_atol(const char *s)
 	return (result * sign);
 }
 
-void	append_node(t_stack **stack, int n)
+static void	append_node(t_stack **stack, int n)
 {
 	t_stack	*node;
 	t_stack	*last_node;
@@ -55,6 +56,7 @@ void	append_node(t_stack **stack, int n)
 	if (!node)
 		return ;
 	node->next = NULL;
+	node->cheapest = 0; // Initialize cheapest
 	node->nbr = n;
 	if (!(*stack))
 	{
@@ -136,3 +138,130 @@ void	prep_for_push(t_stack **stack, t_stack *top_node,
 		}
 	}
 }
+
+// #include "push_swap.h"
+
+// void free_split_array(char **split_array)
+// {
+//     int i = 0;
+//     while (split_array[i] != NULL)
+//     {
+//         free(split_array[i]);
+//         i++;
+//     }
+//     free(split_array);
+// }
+
+// long	ft_atol(const char *s)
+// {
+// 	long	result;
+// 	int		sign;
+
+// 	result = 0;
+// 	sign = 1;
+// 	while (*s == ' ' || *s == '\t' || *s == '\n' || *s == '\r'
+// 		|| *s == '\f' || *s == '\v')
+// 		s++;
+// 	if (*s == '-' || *s == '+')
+// 	{
+// 		if (*s == '-')
+// 			sign = -1;
+// 		s++;
+// 	}
+// 	while (ft_isdigit(*s))
+// 		result = result * 10 + (*s++ - '0');
+// 	return (result * sign);
+// }
+
+// void	append_node(t_stack **stack, int n)
+// {
+// 	t_stack	*node;
+// 	t_stack	*last_node;
+
+// 	if (!stack)
+// 		return ;
+// 	node = malloc(sizeof(t_stack));
+// 	if (!node)
+// 		return ;
+// 	node->next = NULL;
+// 	node->nbr = n;
+// 	if (!(*stack))
+// 	{
+// 		*stack = node;
+// 		node->prev = NULL;
+// 	}
+// 	else
+// 	{
+// 		last_node = find_last(*stack);
+// 		last_node->next = node;
+// 		node->prev = last_node;
+// 	}
+// }
+
+// int init_stack_a(t_stack **a, char **split_av)
+// {
+// 	int i = 0;
+// 	while (split_av[i] != NULL)
+// 	{
+// 		long n;
+// 		char *arg = split_av[i];
+
+// 		if (error_syntax(arg))
+// 		{
+// 			free_split_array(split_av);
+// 			free_errors(a, NULL);
+// 			return -1;
+// 		}
+// 		n = ft_atol(arg);
+// 		if (n > INT_MAX || n < INT_MIN)
+// 		{
+// 			free_split_array(split_av);
+// 			free_errors(a, NULL);
+// 			return -1;
+// 		}
+// 		if (error_duplicate(*a, (int)n))
+// 		{
+// 			free_split_array(split_av);
+// 			free_errors(a, NULL);
+// 			return -1;
+// 		}
+// 		append_node(a, (int)n);
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
+// t_stack	*get_cheapest(t_stack *stack)
+// {
+// 	if (!stack)
+// 		return (NULL);
+// 	while (stack)
+// 	{
+// 		if (stack->cheapest)
+// 			return (stack);
+// 		stack = stack->next;
+// 	}
+// 	return (NULL);
+// }
+
+// void	prep_for_push(t_stack **stack, t_stack *top_node,
+// 	char stack_name)
+// {
+// 	while (*stack != top_node)
+// 	{
+// 		if (stack_name == 'a')
+// 		{
+// 			if (top_node->above_median)
+// 				ra(stack, false);
+// 			else
+// 				rra(stack, false);
+// 		}
+// 		else if (stack_name == 'b')
+// 		{
+// 			if (top_node->above_median)
+// 				rb(stack, false);
+// 			else
+// 				rrb(stack, false);
+// 		}
+// 	}
+// }
